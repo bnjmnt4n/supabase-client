2a77bab9e00795e93e7f6114f2d1135f50052a46
Benjamin Tan <benjamin@dev.ofcr.se>
2021-11-27 18:13:36 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# supabase-client
+
+An alternative Supabase client.
+
+This package intends to experiment with better TypeScript types, and a more tree-shaking-friendly API.
+
+Related: https://github.com/supabase/supabase-js/issues/170.
---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages."${system}";
+      in {
+       devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            nodejs-16_x
+            nodePackages.eslint_d
+            nodePackages.typescript-language-server
+          ];
+        };
+      });
+}
